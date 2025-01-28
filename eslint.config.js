4998b62beae32b6310ca33aaba26e27c984e7398
ferhatmuhamad <ferhatmuhamad221@gmail.com>
2025-01-28 22:07:47 +0700

    Slicing home

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,15 @@
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import prettier from 'eslint-plugin-prettier'
 
 export default [
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
-  },
-
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
-
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  skipFormatting,
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          bracketSameLine: true // Pastikan aturan ini sesuai dengan Prettier
+        }
+      ]
+    }
+  }
 ]
